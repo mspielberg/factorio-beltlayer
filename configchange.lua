fd7ebfe53780f31741c980227790c49fad352a9c
Miles Spielberg <miles@box.com>
2020-07-16 18:02:43 -0700

    fix migration

--- configchange.lua
+++ configchange.lua
@@ -139,43 +139,37 @@ add_migration{
 }
 
 add_migration{
-  name = "v0_5_3_change_simple_entity_with_owner",
-  version = {0,5,3},
+  name = "v0_6_0_change_bpproxies_to_constant_combinator",
+  version = {0,6,0},
   task = function()
-    local reverse_direction = {}
-    for k, v in pairs(defines.direction) do
-      reverse_direction[v] = k
+    local editor = global.editor
+    local prototypes_by_name = game.get_filtered_entity_prototypes{{filter = "transport-belt-connectable"}}
+    local prototype_names = {}
+    for name in pairs(prototypes_by_name) do
+      if name:find("^beltlayer%-bpproxy%-") then
+        prototype_names[#prototype_names + 1] = name
+      end
     end
 
     local function new_proxy_name(entity)
-      local direction = entity.direction
-      if entity.type == "transport-belt" or entity.type == "splitter" then
-        return "beltlayer-"..reverse_direction[direction]..entity.name:sub(#"beltlayer" + 1)
-      elseif entity.type == "underground-belt" then
-        return "beltlayer-"..reverse_direction[direction].."-"..
-          entity.belt_to_ground_type..entity.name:sub(#"beltlayer" + 1)
-      end
-    end
-
-    local editor = global.editor
-    local prototypes_by_name = game.get_filtered_entity_prototypes{{filter="transport-belt-connectable"}}
-    local prototype_names = {}
-    for name in pairs(prototypes_by_name) do
-      if not name:find("^beltlayer%-bpproxy%-") then
-        prototype_names[#prototype_names + 1] = name
-      end
+      return "beltlayer-"..entity.belt_to_ground_type..entity.name:sub(#"beltlayer-")
     end
 
     for _, surface in pairs(game.surfaces) do
       if editor:editor_surface_for_aboveground_surface(surface) then
         for _, entity in pairs(surface.find_entities_filtered{name = prototype_names}) do
-          entity.surface.create_entity{
-            name = new_proxy_name(entity),
-            position = entity.position,
-            force = entity.force,
-            player = entity.last_user,
-          }
-          entity.destroy()
+          entity.order_deconstruction(entity.force, entity.last_user)
+          if entity.type == "underground-belt" then
+            local new_bpproxy = entity.surface.create_entity{
+              name = new_proxy_name(entity),
+              position = entity.position,
+              direction = entity.direction,
+              force = entity.force,
+              player = entity.last_user,
+            }
+            new_bpproxy.order_deconstruction(entity.force, entity.last_user)
+            entity.destroy()
+          end
         end
       end
     end
